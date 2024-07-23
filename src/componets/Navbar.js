7678e37a73275e0d3483849d00cfe69d0b238089
Devkarpenter <deveshkarpenter1998@gmail.com>
2024-07-23 14:22:45 +0530

    css changes

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar(props) {
           <a className="nav-link" href="/">About</a>
         </li>
       </ul>
-          <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+          <div className={`btn form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
           <input className="form-check-input"  onClick={props.togglemode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
           <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
         </div>
