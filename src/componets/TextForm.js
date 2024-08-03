f124d9a98a90ccbcbd16fcdd7523be69bc5ce16a
Devkarpenter <deveshkarpenter1998@gmail.com>
2024-08-03 11:58:05 +0530

    add some function

--- src/componets/TextForm.js
+++ src/componets/TextForm.js
@@ -2,6 +2,20 @@ import React, {useState} from "react";
 import "./TextForm.css"
 
 export default function Textform(props) {
+
+  const fonts = [
+    'Arial, sans-serif',
+    'Courier New, Courier, monospace',
+    'Georgia, serif',
+    'Times New Roman, Times, serif',
+    'Verdana, sans-serif',
+    'Tahoma, sans-serif',
+    'Comic Sans MS, cursive, sans-serif',
+    'Lucida Console, Monaco, monospace'
+  ];
+  
+  const [font, setFont] = useState(fonts[0]);
+
   const handleupClick = () => {
       console.log("Uppercase on " + text);
       let newText = text.toUpperCase();
@@ -25,16 +39,27 @@ export default function Textform(props) {
 
   const [text, setText] = useState('');
 
+  const getRandomFont = () => {
+    const randomIndex = Math.floor(Math.random() * fonts.length);
+    return fonts[randomIndex];
+  };
+
+  // Function to change the font style
+  const changeFontStyle = () => {
+    setFont(getRandomFont());
+  };
+
   return (
     <>
     <div className="bg1">
     <h1>Enter Text -</h1>
     <div className="mb-3">
-      <textarea className="form-control form" value={text} onChange={handleOnChange}  id="mybox" rows="8"></textarea>
+      <textarea className="form-control form" style={{ fontFamily: font }} value={text} onChange={handleOnChange}  id="mybox" rows="8"></textarea>
     </div>
     <button className="button2" onClick={handleupClick}>convert to uppercase</button>
     <button className= "button2" onClick={handleloClick}>convert to lowercase</button>
     <button className= "button2" onClick={handleclearClick}>clear all</button>
+    <button className= "button2" onClick={changeFontStyle}>Fonts style</button>
     </div>
     <div class="container">
     <h1>Your Text Summary</h1>
